3e1889c2f10a4e28f4758b893ae1dfbd49621fc0
Lindsey <lmdragun@gmail.com>
2015-04-30 18:04:19 -0400

    experimenting with fading and a stop button

--- slidey_js.js
+++ slidey_js.js
@@ -1,21 +1,46 @@
+$(document).ready(function(){ });
+
 var imageSpace = document.getElementById("background");
 var imageCollection = ["http://i.imgur.com/oq98hTA.jpg", "http://i.imgur.com/Sl8CPMP.jpg", "http://i.imgur.com/hG9Xb.jpg", "http://i.imgur.com/GYEse.jpg", "http://i.imgur.com/ndDhksZ.jpg"];
-var random = Math.floor(Math.random() * 4);
-console.log(random);
+// var random = Math.floor(Math.random() * 4);
+// console.log(random);
 i = 0;
 
-backgroundImage();
-
-setInterval(function () {
-		i++;
-		if(i >= imageCollection.length){
-			i = 0;
-		}
-		backgroundImage();
-}, 1000);
-
-console.log(imageCollection[1])
-
 function backgroundImage(){
-$("#background").css('background-image', 'url(' + imageCollection[i] + ')');
-};
+	imageAmount();
+	$("#background").css('background-image', 'url(' + imageCollection[i] + ')');
+	i++;
+}
+
+var imageMovement = setInterval(function () {
+		backgroundImage();
+	}, 1000);
+
+function imageAmount(){
+	if(i >= imageCollection.length){
+		i = 0;
+	}
+}
+
+function backgroundFadeIn(){
+	$("#background").fadeIn(4000);
+}
+
+function backgroundFadeOut(){
+	$("#background").fadeOut(4000);
+}
+
+function stopShow(){ setTimeout(function () {
+			clearInterval(imageMovement);
+	}, 1000);}
+
+// $(".stop").click(stopShow());
+
+// function backgroundImage(){
+// 	setInterval(function(){
+// 	jQuery.each( imageCollection, function( i, val ) {
+// 		$("#background").css('background-image', 'url(' + val + ')').fadeIn(4000);
+// 		console.log(i+ ":" + val);
+// 	});
+// }, 1000);
+// }
