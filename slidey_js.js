a96d132073ad09da4495987dcc95474fd37e5fa7
presto2116 <preston.straight@gmail.com>
2015-05-01 09:16:12 -0400

    fixed a broken variable

--- slidey_js.js
+++ slidey_js.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
 var images = ["http://i.imgur.com/wcbCIc3.jpg", "http://i.imgur.com/2tQTR7t.jpg","http://i.imgur.com/ejbGEh2.gif", "http://i.imgur.com/u0tOBYz.jpg", "http://i.imgur.com/VATK187.jpg"];
 
 var backer = $("#background");
-i = 0;
+
+var i = 0;
+
 var changeImage = function(){
 	startOver();
 	backer.css("background-image", "url("+ images[i] + ")").fadeIn(600).delay(1800).fadeOut(600); i++;
-
 }
 
 function startImages() {
