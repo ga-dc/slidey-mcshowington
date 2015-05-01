14aa30e07825d1f9e2acac0920e1bb17c35ef07d
Aaron <aaronpark87@gmail.com>
2015-04-30 23:00:30 -0400

    w07d04

--- slidey_js.js
+++ slidey_js.js
@@ -2,17 +2,18 @@
 
 $(document).ready(function(){
 
-img = ["http://i.imgur.com/UNDvImp.jpg",
+	img = ["http://i.imgur.com/UNDvImp.jpg",
      "http://i.imgur.com/FL1cDRZ.jpg",
      "http://i.imgur.com/WvvpdVO.jpg",
      "http://i.imgur.com/CrrJfqr.jpg",
      "http://i.imgur.com/bToQoi7.jpg"];
 
-i = 0;
+	i = 0;
 
-setInterval(function(){
+	slideShow = setInterval(function(){
         	// set background img via css
 			$("#background").css("background-image", "url("+ img[i] +")");
+
         	// loop image array
         	i++;
         	if (i === img.length){
@@ -20,5 +21,13 @@ setInterval(function(){
         	}
         }
         // set interval counter
-    	, 3000);
-	)};		// don't understand the need for this ")}"
+    	, 5000);
+	// stop cycle on click
+	$("#background").on("click", function(){
+		clearInterval(slideShow);
+	// $("#background").on("click", function(){
+	// 	slideShow;
+	//});
+	});
+
+});
