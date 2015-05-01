1203d64e0096efd7774641b810b95f01fa64bfce
Enoch Shin <seekpeace@gmail.com>
2015-05-01 08:48:18 -0400

    fixing background spelling

--- slidey_js.js
+++ slidey_js.js
@@ -1,10 +1,11 @@
+$(document).ready(function(){
 
 var i = 0;
-var arrayPictures = ["http://imgur.com/gallery/R2rNVh6","http://imgur.com/gallery/pJlBJCU","http://imgur.com/gallery/bytheWT","http://imgur.com/gallery/YAZxVlq","http://imgur.com/gallery/z1mjC9p"];
+var arrayPictures = ["http://i.imgur.com/8OaXnmv.png","http://i.imgur.com/z1mjC9p.jpg","http://imgur.com/fR2lRfW?tags","http://imgur.com/pJlBJCU","http://imgur.com/bytheWT","http://imgur.com/YAZxVlq"];
 
 function startSlide(){
   // $("#backgroud").css('background','url(' + arrayPictures[i] + ')');
-  $("#backgroud").css('background','url('+arrayPictures[i]+')').fadeOut("slow");
+  $("#background").css('background-image','url('+arrayPictures[i]+')').fadeToggle(2000);
     i++;
     if (i > 5) {
       i = 0;
@@ -22,3 +23,5 @@ startSlide();
 // $('myOjbect').css('background-image', 'url(' + imageUrl + ')');
 
 // http://stackoverflow.com/questions/512054/setting-background-image-using-jquery-css-property
+
+});
