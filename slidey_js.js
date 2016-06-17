67794b6900a654ae4b49fca945e14f7fa0390551
Denzel Christopher Adams <Blvckyuppie@gmail.com>
2016-06-17 08:57:26 -0400

    minimum viable product

--- slidey_js.js
+++ slidey_js.js
@@ -1,30 +1,33 @@
-txtBox = word.val();
-
-
 $(document).ready(function(){
-  var url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg&tag=";
-  var background = $("#background");
-  var word = $("#word");
-  function getTxtBox(txtBox){
-    // txtBox = word.val();
+
+  var domStuff = {
+    url : "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg&tag=",
+    background : $("#background"),
+    word : $("#word"),
+  }
+
+  function setBackground(response) {
+    var img = response["data"]["image_url"];
+    domStuff.background.css("background-image", "url("+ img +")");
+  }
+
+  function getTxtBox(){
+    txtBox = domStuff.word.val();
     console.log(txtBox);
     debugger;
   }
-  function setBackground(response) {
-    var img = response["data"]["image_url"];
-    console.log(img);
-    background.css("background-image", "url("+ img +")");
-  }
-  word.keydown(function(el){
+
+  domStuff.word.keydown(function(el){
     console.log(el)
     if (el.keyCode === 13) {
       console.log("this works")
       $.ajax({
-        url: url,
+        url: domStuff.url,
         type: "GET",
         dataType: "json",
+
       }).done(function(response){
-        getTxtBox(txtBox);
+        getTxtBox();
         debugger;
         setBackground(response);
         debugger;
